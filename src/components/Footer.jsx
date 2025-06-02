da78e30cf48ce01b76fd7b86061ed9dba7b41d75
Sai Pranitha Gunti <148933492+pranithagunti@users.noreply.github.com>
2025-06-02 15:28:40 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+// src/components/Footer.jsx
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      © {new Date().getFullYear()} SAI PRANITHA GUNTI. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
